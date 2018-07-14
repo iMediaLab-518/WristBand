51f964a2855e4d657be2af3e21ec6be5f536603b
ls1rius <865212941@qq.com>
2018-07-14 14:16:37 +0800

    no message

--- bluetoothTest.py
+++ bluetoothTest.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
 #--*--coding=utf-8--*--
-import time
-from bluetooth import *
-alreadyFound = []
-def findDevs():
-    foundDevs = discover_devices(lookup_names=True)
-    for(addr,name) in foundDevs:
-        if addr not in alreadyFound:
-            print ("[*] Found Bluetooth Device :  " +str(name))
-            print ("[+] MAC address :  " +str(addr))
-            alreadyFound.append(addr)
+# import time
+# from bluetooth import *
+# def rfcommCon(addr,port):
+#     sock = BluetoothSocket(RFCOMM)
+#     try:
+#         sock.connect((addr,port))
+#         print "[+] RFCOMM port : " +str(port)+' open'
+#         sock.close()
+#     except Exception,e:
+#         print '[-] RFCOMM port :' +str(port)+' closed'
  
-while True:
-    findDevs()
-    time.sleep(5)
+# for port in range(1,30):
+#     rfcommCon('98:E9:2A:B9:XX:57',port)
+
+
+
+
+# import time
+# from bluetooth import *
+# alreadyFound = []
+# def findDevs():
+#     foundDevs = discover_devices(lookup_names=True)
+#     for(addr,name) in foundDevs:
+#         if addr not in alreadyFound:
+#             print "[*] Found Bluetooth Device :  " +str(name)
+#             print "[+] MAC address :  " +str(addr)
+#             alreadyFound.append(addr)
+ 
+# while True:
+#     findDevs()
+#     time.sleep(5)
