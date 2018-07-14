0bfda0dc1e5a4d297dd98a12be5e88b3591d51e2
ls1rius <865212941@qq.com>
2018-07-14 15:35:39 +0800

    wrist get data test

---
+++ wristTest.py
@@ -0,0 +1,39 @@
+import blescan
+import sys
+import time
+
+import bluetooth._bluetooth as bluez
+
+dev_id = 0
+sock = bluez.hci_open_dev(dev_id)
+blescan.hci_le_set_scan_parameters(sock)
+blescan.hci_enable_le_scan(sock)
+
+measured_anterior = 0
+
+try:
+    while True:
+        returnedList = blescan.parse_events(sock, 1)
+        if len(returnedList) > 0:
+            (mac, uuid, major, minor, txpower, rssi) = returnedList[0].split(',', 6)
+            # CAMBIAR LA DIRECCION MAC
+            if mac == 'CC:50:98:E9:2A:B9':
+                print("get this device")
+                print(uuid)
+                measunit = uuid[22:24]
+                measured = int((uuid[26:28] + uuid[24:26]), 16) * 0.01
+
+                unit = ''
+
+                if measunit.startswith(('03', 'b3')): unit = 'lbs'
+                if measunit.startswith(('12', 'b2')): unit = 'jin'
+                if measunit.startswith(('22', 'a2')): unit = 'Kg' ; measured = measured / 2
+
+                if unit:
+                    if measured != measured_anterior:
+                        print("measured : %s %s" % (measured, unit))
+                        measured_anterior = measured
+
+
+except KeyboardInterrupt:
+    sys.exit(1)
