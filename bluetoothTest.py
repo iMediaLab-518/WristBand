6133e664ad1614d49ae9b3e3c60a8b53e637112c
ls1rius <865212941@qq.com>
2018-07-14 12:30:49 +0800

    no message

--- bluetoothTest.py
+++ bluetoothTest.py
@@ -7,8 +7,8 @@ def findDevs():
     foundDevs = discover_devices(lookup_names=True)
     for(addr,name) in foundDevs:
         if addr not in alreadyFound:
-            print "[*] Found Bluetooth Device :  " +str(name)
-            print "[+] MAC address :  " +str(addr)
+            print ("[*] Found Bluetooth Device :  " +str(name))
+            print ("[+] MAC address :  " +str(addr))
             alreadyFound.append(addr)
  
 while True:
