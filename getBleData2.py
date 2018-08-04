24e5f0070b46e89edc15c6858f08c9f121f1db9b
ls1rius <865212941@qq.com>
2018-08-04 19:44:15 +0800

    no message

--- getBleData2.py
+++ getBleData2.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     # for item in ble_conn.getCharacteristics(startHnd=1, endHnd=0xFFFF,uuid='fee7'):
     #     print("characteristics:",item)
-    ch = ble_conn.getCharacteristics(uuid='6e400003-b5a3-f393-e0a9-e50e24dcca9e')
+    ch = ble_conn.getCharacteristics(uuid='fec9')
     
     print(ch)
 
