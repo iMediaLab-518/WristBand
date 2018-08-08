7e3c4790937123e5cd08bf1876af7a580ab3cd68
ls1rius <865212941@qq.com>
2018-08-08 10:38:22 +0800

    no message

--- getBleData2.py
+++ getBleData2.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
 
 
         ch = item
-        print(ch)
+        print("tmp-frist: ",tmp)
 
         snd_content_str = "0x0506"
         tmp = ble_conn.writeCharacteristic(ch.getHandle(), snd_content_str)
