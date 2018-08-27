e71bfba827595719418b197df075fd9aefa38353
ls1rius <865212941@qq.com>
2018-08-27 12:05:57 +0800

    no message

--- getBleData3.py
+++ getBleData3.py
@@ -88,6 +88,10 @@ if __name__ == '__main__':
         snd_content_str = "0x05060700010007"
         tmp = ble_conn.writeCharacteristic(ch.getHandle(), snd_content_str)
 
+
+        print(tmp)
+        
+        print("--------------")
         # print("tmp: ",tmp)
         
         # print("ch: ",ch)
