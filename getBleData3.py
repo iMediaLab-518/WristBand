9da9d00cbdab05dcc81677ee231d0056cc6a23ed
ls1rius <865212941@qq.com>
2018-08-29 12:03:32 +0800

    no message

--- getBleData3.py
+++ getBleData3.py
@@ -74,11 +74,11 @@ if __name__ == '__main__':
 
                 ch.write(val = b"0x05060700010007",withResponse=True)
 
-                # ble_conn.waitForNotifications(10.0)
+                ch.peripheral.waitForNotifications(10.0)
 
 
     # wait notification  
-    ble_conn.waitForNotifications(10.0)
+    # ble_conn.waitForNotifications(10.0)
     
     # disconnect 
     ble_disconnect()
