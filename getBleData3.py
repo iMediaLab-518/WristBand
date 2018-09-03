5aa8fcb19cf688abd1b0e7c2fcc2d77f53583426
ls1rius <865212941@qq.com>
2018-09-03 10:29:39 +0800

    no message

--- getBleData3.py
+++ getBleData3.py
@@ -67,26 +67,28 @@ if __name__ == '__main__':
     ble_connect(ble_mac)
     # write , set listen
 
-    # ch = ""
+    ch = ""
 
-    # for item in ble_conn.getServices():
-    #     # print("services:",item.uuid)
-    #     if item.uuid == "be940000-7333-be46-b7ae-689e71722bd5":
-    #         # print(item.uuid)
-    #         for iitem in item.getCharacteristics("be940001-7333-be46-b7ae-689e71722bd5"):
+    for item in ble_conn.getServices():
+        # print("services:",item.uuid)
+        if item.uuid == "be940000-7333-be46-b7ae-689e71722bd5":
+            # print(item.uuid)
+            for iitem in item.getCharacteristics("be940001-7333-be46-b7ae-689e71722bd5"):
 
-    #             ch = iitem
+                ch = iitem
 
-    #             break
+                print(ch)
 
-    #             # ch.write(val = b"0x05060700010007",withResponse=True)
+                break
 
-    #             # ch.peripheral.waitForNotifications(10.0)
+                # ch.write(val = b"0x05060700010007",withResponse=True)
+
+                # ch.peripheral.waitForNotifications(10.0)
 
 
     i=0
 
-    ble_conn.writeCharacteristic(39, b"0x05060700010007",withResponse=True)
+    ble_conn.writeCharacteristic(ch.getHandle(), b"0x05060700010007",withResponse=True)
 
     while(True):
         if(ble_conn.waitForNotifications(2.0)):
